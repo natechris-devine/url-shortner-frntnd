be08104ec09c3f8cee4e5172d3c2c9e225f21ea7
NateChris <divineconclusion@gmail.com>
2020-08-26 03:07:05 -0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href='#'>NC SUPER Shortner</a>
+    </nav>
+    <div class="container">
+      <home />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "./components/Home.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<div class="row">
+			<div class="col col-12 offset-0 mt-2">
+					<h1 class="jumbotron text-center text-white bg-primary">Create Click-Worthy Links</h1>
+			</div>
+		</div>
+
+		<div class="col col-8 aligh-middle mt-5 offset-2">
+			<div class="card">
+				<div class="card-body">
+					<form @submit.prevent="submit(url)">
+						<div class="form-group">
+							<label for="url">Enter Url</label>
+							<textarea type="url" class="form-control" v-model="url" style="height:150px" />
+						</div>
+						<div class="form-group" v-show="shortUrl">
+							<p>
+								Short URL: :
+								<a :href="shortUrl" class="text-primary">{{shortUrl}}</a>
+							</p>
+						</div>
+						<div class="form-group">
+							<button class="btn btn-primary" type="submit">Shorten URL</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
