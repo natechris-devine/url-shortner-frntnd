9606b0dfece381f21d0ff88d8ba381332fbd2cae
NateChris <divineconclusion@gmail.com>
2020-08-26 03:19:47 -0500

    Complete Frontend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -29,3 +29,28 @@
 		</div>
 	</div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+	data: () => {
+		return {
+			url: "",
+			shortUrl: "",
+		};
+	},
+	methods: {
+		submit: async function (url) {
+			try {
+				const api = "http://localhost:5000/url";
+				const response = await axios.post(api, {
+					url,
+				});
+				this.shortUrl = response.data.shortUrl;
+			} catch (error) {
+				console.log(error);
+			}
+		},
+	},
+};
+</script>
